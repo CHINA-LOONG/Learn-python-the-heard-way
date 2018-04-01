0b8d7054b9cd613aa4281ec04fd6188a648afaa9
china_loong <xuelong9009@qq.com>
2018-04-01 23:13:43 +0800

    示例19：函数参数的作用域

---
+++ ex19.py
@@ -0,0 +1,16 @@
+
+
+def cheese_and_crackers(cheese_count, boxs_of_crackers):
+	print cheese_count
+	print boxs_of_crackers
+
+cheese_and_crackers(20,30)
+
+amount_cheese = 10
+amount_crackers = 50
+
+cheese_and_crackers(amount_cheese,amount_crackers)
+
+cheese_and_crackers(10+12, 20+3-1)
+
+cheese_and_crackers(amount_cheese+100, amount_crackers+100)
