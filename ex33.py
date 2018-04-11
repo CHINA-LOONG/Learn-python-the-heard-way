c380ada732966480b60e1f2a770f9878ce036627
DESKTOP-N9OOS8B\xuelo <xuelong9009@qq.com>
2018-04-11 20:17:27 +0800

    示例33：while循环学习

---
+++ ex33.py
@@ -0,0 +1,19 @@
+i = 0
+
+numbers = []
+
+while i < 10:
+	print "At the top i is %d" % i
+	numbers.append(i)
+
+	i+=1
+	break
+	print "Numbers new:", numbers
+	print "At the bottom i is %d" % i
+
+print "The numbers:"
+
+for num in numbers:
+	print num
+
+
