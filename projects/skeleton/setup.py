44d956aa8b763b184def7550ed925b2bd3a1aad2
china_loong <xuelong9009@qq.com>
2018-04-25 16:36:26 +0800

    exercise46:创建基数项目框架，调用测试nosetests

---
+++ projects/skeleton/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    print "11"
+
+config = {
+    'description': 'My Project',
+    'author': 'My Name',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'NAME'
+}
+
+setup(**config)
