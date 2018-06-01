e5ad4d905f65a57dadf8a4fa2697c8efc00ee473
DESKTOP-N9OOS8B\xuelo <xuelong9009@qq.com>
2018-06-01 15:59:17 +0800

    温习40-46 ，module 、class、object

--- ex40.py
+++ ex40.py
@@ -1,17 +1,18 @@
 class Song(object):
-	"""docstring for Song"""
-	def __init__(self, lyrics):
-		super(Song, self).__init__()
-		self.lyrics = lyrics
-	
-	def sing_me_a_song(self):
-		for line in self.lyrics:
-			print line
+    """docstring for Song"""
 
-happy_bday=Song(["Happy birthday to you",
-				 "I dont want to get sued"])
+    def __init__(self, lyrics):
+        super(Song, self).__init__()
+        self.lyrics = lyrics
 
-bulls_on_parade=Song(["They rally around tha family"])
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
+
+happy_bday = Song(["Happy birthday to you",
+                   "I dont want to get sued"])
+
+bulls_on_parade = Song(["They rally around tha family"])
 
 happy_bday.sing_me_a_song()
 
