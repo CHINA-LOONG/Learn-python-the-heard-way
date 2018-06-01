3818112226304fd7d7d803dc25baac5ab0a26e89
DESKTOP-N9OOS8B\xuelo <xuelong9009@qq.com>
2018-06-01 10:48:47 +0800

    温习15-26 exercise

--- ex18.py
+++ ex18.py
@@ -1,20 +1,31 @@
 
 # this one is like your script with argv
+
+
+def print_twostr(*args):
+    arg1, arg2 = args
+    print "arg1 = %s , arg2 = %s." % (arg1, arg2)
+
+
 def print_two(*args):
-	arg1, arg2 = args
-	print "arg1 = %r ,arg2 = %r ." % (arg1, arg2)
+    arg1, arg2 = args
+    print "arg1 = %r ,arg2 = %r ." % (arg1, arg2)
+
 
 def print_two_param(arg1, arg2):
-	print "arg1 = %r ,arg2 = %r ." % (arg1, arg2)
+    print "arg1 = %r ,arg2 = %r ." % (arg1, arg2)
+
 
 def print_one(arg1):
-	print "arg1 = %r ." % arg1
+    print "arg1 = %r ." % arg1
+
 
 def print_none():
-	print "nothing ."
+    print "nothing ."
 
 
+print_twostr("hello", "world")
 print_two("hello", "world")
-print_two_param("hello", "python")	
+print_two_param("hello", "python")
 print_one("this is one")
 print_none()
--- ex21.py
+++ ex21.py
@@ -1,15 +1,13 @@
 
 
-
-
 def add(a, b):
-	print "Adding %d +%d" % (a, b)
-	return a + b
+    print "Adding %d +%d" % (a, b)
+    return a + b
 
 
 age = add(10, 5)
 
 
-what = add(age, 5)
+what = add(100, add(age, 5))
 
 print "result:%d" % what
